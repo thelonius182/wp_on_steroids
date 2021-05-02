ad324fe1a36c14a3f6297c25c38fc88d3d0f54d2
thelonius182 <lcavdakker@gmail.com>
2021-05-02 18:22:22 +0200

    tested & deployed as task

--- src/cz_monitor.R
+++ src/cz_monitor.R
@@ -1,5 +1,9 @@
-# 
-# WP-on-steroids
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+#
+# Script: WP-on-steroids 
+# Version 1.0: CZ/LvdA, 2021-05-01
+#
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
   
 library(digest)
 library(readr)
@@ -9,63 +13,104 @@ library(yaml)
 library(lubridate)
 library(magrittr)
 library(stringr)
+library(keyring)
+library(RMySQL)
+library(tibble)
 
+# init functions ----
 source("src/wpos_utilities.R", encoding = "UTF-8")
 
-# init cfg
+# init cfg ----
 wpos_config <- read_yaml("config.yaml")
 
-# init logging
+# init logging ----
 path.wpos_log <- paste(wpos_config$log_home, "wpos.log", sep = "/")
 lg_ini <- flog.appender(appender.file(path.wpos_log), "wpos")
 flog.info("= = = = = WP-monitor START = = = = =", name = "wpos")
 
-# set file to watch
-# - for production: manually set testfile = none
-if (wpos_config$test_file == "none") {
-  
-  wpos_today <- now(tzone = "Europe/Amsterdam")
-  # change hour to start-of-broadcast 
-  hour(wpos_today) <- wpos_config$broadcast_start_hour
-  # build doc-name that will hold the playlist tracks
-  pl_name <- format_ISO8601(wpos_today, precision = "ymdh") %>% 
-    str_remove_all("-") %>% 
-    str_replace("T", "_") %>% 
-    paste0(".txt")
-  path.playlist <- paste(wpos_config$dir_to_watch, pl_name, sep = "/")
-  
-  if (!file_exists(path = path.playlist)) {
-    file_create(path = path.playlist)
-  }
-  
-} else {
-  path.playlist <- paste(wpos_config$dir_to_watch, wpos_config$test_file, sep = "/")
+# init file to watch ----
+wpos_today <- now(tzone = "Europe/Amsterdam")
+# change hour to start-of-broadcast
+hour(wpos_today) <- wpos_config$broadcast_start_hour
+
+# build doc-name that will hold the playlist tracks
+pl_name <- format_ISO8601(wpos_today, precision = "ymdh") %>%
+  str_remove_all("-") %>%
+  str_replace("T", "_") %>%
+  paste0(".txt")
+path.playlist <- paste(wpos_config$dir_to_watch, pl_name, sep = "/")
+
+if (!file_exists(path = path.playlist)) {
+  file_create(path = path.playlist)
 }
 
-# create rest of start situation
-path.playlist_lock <- paste(wpos_config$dir_to_watch, ".lock", sep = "/")
-file_create(path = path.playlist_lock)
+# init file hash ----
 playlist <- read_lines(file = path.playlist)
 playlist.md5 <- digest(object = playlist)
 playlist.md5_sav <- playlist.md5
+
+# init polling interval ----
 POLL_INTERVAL <- wpos_config$polling_interval_in_secs
 
-# start monitoring, every POLL_INTERVAL seconds
-flog.info(paste("now watching: ", path.playlist), name = "wpos")
+# init stop-time ----
+wpos_stop <- now(tzone = "Europe/Amsterdam")
+hour(wpos_stop) <- wpos_config$mon_stop_h
+minute(wpos_stop) <- wpos_config$mon_stop_m
+second(wpos_stop) <- 0L
 
-while (file_exists(path = path.playlist_lock)) {
+# single "for"-loop provides points of exit
+for (seg2 in 1:1) {
   
-  # get current file contents & hash
-  playlist <- read_lines(file = path.playlist)
-  playlist.md5 <- digest(object = playlist)
+  # Connect to database ----
+  flog.info("trying to connect to WordPress-DB ...", name = "wpos")
+  wp_conn <- get_wp_conn()
   
-  # file non-empty and different hash: update WordPress
-  if (file_size(path = path.playlist) > 0 && playlist.md5 != playlist.md5_sav) {
-    playlist.md5_sav <- playlist.md5
-    update_wp(playlist)
+  # connection-type=S4 indicates a valid connection; other types indicate failure
+  if (typeof(wp_conn) != "S4") {
+    break
   }
   
-  Sys.sleep(time = POLL_INTERVAL)
-}
+  flog.info("OK ... connection established", name = "wpos")
+  
+  sql_post_key <- str_remove(pl_name, "\\.txt")
+  
+  upd_stmt01 <- sprintf("SELECT cz_id FROM cz.salsa_plws_gidsweek where pgmStart = '%s';", sql_post_key)
+  
+  dsSql01 <- dbGetQuery(wp_conn, upd_stmt01)
+  
+  # === TESTESTEST !!!
+  # dsSql01$cz_id <- 432782 # Framework 2018-02-26 05:00:00
+  # === TESTESTEST !!!
+  
+  # start monitoring ----
+  flog.info(paste("watching", path.playlist, "until", wpos_stop, "id =", dsSql01$cz_id), name = "wpos")
+  
+  while (TRUE) {
 
+    # + game over ----
+    if (now(tzone = "Europe/Amsterdam") > wpos_stop) {
+      flog.info("stopping ...", name = "wpos")
+      break
+    }
+    
+    # + get current content ----
+    playlist <- read_lines(file = path.playlist)
+    playlist.md5 <- digest(object = playlist)
+    
+    # + update on change ----
+    if (file_size(path = path.playlist) > 0 && playlist.md5 != playlist.md5_sav) {
+      playlist.md5_sav <- playlist.md5
+      update_wp()
+    }
+    
+    # + wait a few moments ----
+    Sys.sleep(time = POLL_INTERVAL)
+  }
+  
+  # disconnect from DB
+  dbDisconnect(wp_conn)
+  flog.info("disconnected from WordPress-DB", name = "wpos")
+}
+  
+# monitoring stopped----
 flog.info("= = = = = WP-monitor STOP  = = = = =", name = "wpos")
--- src/wpos_utilities.R
+++ src/wpos_utilities.R
@@ -1,9 +1,19 @@
-update_wp <- function(cz_playlist) {
-  flog.info(paste("file updated:", playlist.md5), name = "wpos")
+playlist2postdate <- function(playlist) {
+  tmp_date <- playlist %>% str_sub(0, 8)
+  tmp_time <- playlist %>% str_sub(11, 13) %>% paste0(":00:00")
+  result <- paste0(tmp_date, " ", tmp_time) %>% ymd_hms
 }
 
-get_wp_conn <- function(wp_env) {
-  db_type <- wp_env
+get_wallclock <- function(pm_cum_tijd, pm_playlist) {
+  cum_tijd_ts <- paste0("2019-01-01 ", pm_cum_tijd) %>% ymd_hms
+  start_clock <- pm_playlist %>% str_sub(12, 13) %>% as.integer
+  wallclcok_ts <- cum_tijd_ts + hours(start_clock)
+  wallclock_ts_rounded <- wallclcok_ts %>% round_date("minute")
+  wallclock <- wallclock_ts_rounded %>% as.character %>% str_sub(12, 16)
+}
+
+get_wp_conn <- function() {
+  db_type <- "prd"
   db_host <- key_get(service = paste0("sql-wp", db_type, "_host"))
   db_user <- key_get(service = paste0("sql-wp", db_type, "_user"))
   db_password <- key_get(service = paste0("sql-wp", db_type, "_pwd"))
@@ -12,13 +22,39 @@ get_wp_conn <- function(wp_env) {
   db_table <- "cz.wp_posts"
   
   result <- tryCatch( {
-    wp_conn_prd <- dbConnect(drv = MySQL(), user = db_user, password = db_password,
-                             dbname = db_name, host = db_host, port = db_port)
-    },
-    error = function(cond) {
-      flog.error("Wordpress database onbereikbaar (dev: check PuTTY)", name = "wpos")
-      return("connection-error")
-    }
+    grh_conn <- dbConnect(drv = MySQL(), user = db_user, password = db_password,
+                          dbname = db_name, host = db_host, port = db_port)
+  },
+  error = function(cond) {
+    flog.error("Wordpress database onbereikbaar (dev: check PuTTY)", name = "wpos")
+    return("connection-error")
+  }
   )
   return(result)
+}
+
+update_wp <- function() {
+
+  # standard summary
+  sql_gidstekst <- "Experimenteel, avant-garde, industrial, ambient en electronics.\n<!--more-->\n&nbsp;\n&nbsp;\n"
+  
+  # tracks
+  tib_pl <- playlist %>% as_tibble()
+  names(tib_pl) <- "pl_line"
+  
+  for (cur_pl_line in tib_pl$pl_line) {
+
+    sql_gidstekst <- paste0(sql_gidstekst,
+                            dbEscapeStrings(wp_conn, enc2native(cur_pl_line)),
+                            "\n")
+  }
+  
+  upd_stmt02 <-
+    sprintf("update wp_posts set post_content = '%s' where id = %i;",
+            sql_gidstekst,
+            dsSql01$cz_id)
+  
+  dbGetQuery(wp_conn, upd_stmt02)
+  
+  flog.info("Gids bijgewerkt: %s", pl_name, name = "wpos")
 }
